528ae8ff7bd05049fd6624fed7963f3ca26aadfe
Marco <marco.gnecchi@gmail.com>
2013-02-16 19:33:06 +0000

    Added style and a raw dashboard

--- src/it/manusaservices/activities/DashBoard.java
+++ src/it/manusaservices/activities/DashBoard.java
@@ -1,13 +1,46 @@
 package it.manusaservices.activities;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import it.manusaserveces.activities.R;
 
-public class DashBoard extends ManusaActivity {
+public class DashBoard extends ManusaActivity implements ActionBar.TabListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.dashboard);
+    
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("foo").setTabListener(this));
+
+    
+    
+    
     }
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
 }
